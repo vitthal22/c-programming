fc78722322b833ec5c511ddabb6e4b80a89f398e
vitthal gupta <vitthalgupta47171@gmail.com>
2024-05-23 23:11:56 +0530

    commit

---
+++ control_decision_making_statement.c
@@ -0,0 +1,258 @@
+#include<stdio.h>
+int main()
+{
+    // int age;
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+
+    // printf("age is 18\n");
+    // printf("age is greater than 60\n");
+    // printf("invalid age\n");
+
+    /*
+    we use desion making statement to control the code in the above one because it will exceute the whole 
+    code line wise line so we use control- flow statement
+    */
+
+     /*
+     syntax
+     if(condition)  //age == 18
+     {
+        some block of code
+    }
+     */
+
+    // if(age == 18)
+    // {
+    //     printf("age is 18\n");
+    // }
+
+    // if (age == 18)
+    // {
+    //     printf("age is 18\n");
+    // }
+    // else
+    // {
+    //     printf("age is not 18");
+    // }
+    
+    // if (age == 18)
+    // {
+    //     printf("age is 18\n");
+    // }
+    // else if (age==60)
+    // {
+    //     printf("age is 60\n");
+    // }
+    // else if (age == 100)
+    // {
+    //     printf("age is 100\n");
+    // }
+    // else
+    // {
+    //     printf("invalid age\n");
+    // }
+
+    //task
+
+    // age is greater than 18 and 60 and less
+
+    // if (age>= 18 && age<=60)
+    // {
+    //     printf("you are eligible");
+    // }
+    // else if (age>=60 && age<=100)
+    // {
+    //     printf("you are not eligible");
+    // }
+    // else
+    // {
+    //     printf("invalid age");
+    // }
+
+    // if ((age<18)||(age>60 && age<100))
+    // {
+    //     printf("you are not eligible");
+    // }
+    // else if (age>=18&&age<=60)
+    // {
+    //     printf("you are eligible");
+    // }
+    // else
+    // {
+    //     printf("invalid age");
+    // }
+
+    // int number;
+    // printf("enter a number:");
+    // scanf("%d", &number);
+
+    // if (number%2 ==0)
+    // {
+    //     printf("even no.");
+    // }
+    // else
+    // {
+    //     printf("odd no.");
+    // }
+
+    // int num;
+    // printf("enter num: ");
+    // scanf("%d", &num);
+
+    // if (num>11)
+    // {
+    //     printf("no. is greater than 11");
+    // }
+    
+    // int cost_price;
+    // int selling_price;
+    // printf("enter cost:");
+    // scanf("%d", &cost_price);
+    // printf("enter selling price:");
+    // scanf("%d", &selling_price);
+
+    // if (selling_price-cost_price>0)
+    // {
+    //     printf("profit");
+    // }
+    // else
+    // {
+    //     printf("loss");
+    // }
+
+
+    // if-else nested
+    
+
+    // int marks;
+    // printf("enter marks: ");
+    // scanf("%d", &marks);
+
+    // if (marks>=60 && marks<75)
+    // {
+    //     printf("pass with first class\n");
+    //     int choice;
+    //     printf("enter choice: ");
+    //     scanf("%d", &choice);
+
+    //     if (choice == 1)
+    //     {
+    //         printf("iphone");
+    //     }
+    //     else if (choice == 2)
+    //     {
+    //         printf("samsung");
+    //     }
+    //     else
+    //     {
+    //         printf("BETTer luck next time");
+    //     }
+        
+    // }
+    // else if (marks>=75 && marks<=100)
+    // {
+    //     printf("first class with distinction\n");
+    // }
+    // else
+    // {
+    //     printf("pass\n");
+    // }    
+    
+
+
+    // char letter;
+    // printf("enter alphabet: ");
+    // scanf("%c", &letter);
+
+    // if (letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u')
+    // {
+    //     printf("vowel");
+    // }
+    // else
+    // {
+    //     printf("consonant");
+    // }
+    
+    // int x=0,y=10,z=5;
+    // if (x||y)
+    // {
+    //     printf("z =%d", z);
+    // }
+    
+
+    // Switch statement
+
+// break is only used in switch and loop cases.
+
+
+    // int marks;
+    // printf("enter marks: ");
+    // scanf("%d", &marks);
+
+    // switch (marks)
+    // {
+    // case 75:{
+    //     printf("maeks is 75");
+    //     break;
+    // }
+    // case 60:{
+    //     printf("maeks is 60");
+    //     break;
+    // }
+    // default:
+    // {
+    //     printf("invalid marks");
+    // }
+    //     break;
+    // }
+
+
+    //Class exercise
+
+    // int marks;
+    // printf("enter marks: ");
+    // scanf("%d", &marks);
+
+    // if (marks>=75) printf("grade A");
+    // else if (marks>=60) printf("grade B");
+    // else if (marks>=40) printf("grade C");
+    // else printf("grade f");
+    
+
+    //  int year;
+    //  printf("enter year: ");
+    //  scanf("%d", &year);
+    //  if ((year%4==0 && year%100!=0)|| year%400==0)
+    //  {
+    //     printf("leap year");
+    //  }
+    //  else printf("not leap year");
+
+    // int num;
+    // printf("enter num: ");
+    // scanf("%d", &num);
+    // (num%2==0)?(printf("even")):(printf("odd"));
+
+    
+
+
+
+
+
+    
+
+
+    
+    
+    
+     
+    
+
+
+    
+
+
+
+    return 0;
+}
---
+++ loop_1.c
@@ -0,0 +1,261 @@
+//loops_1
+
+// loops is used to write the code in repeated block of code.
+
+/*
+for loop
+while loop
+do-while loop
+*/
+
+/*
+for loop
+syntax
+for (initialization; condition; increment/decrement)
+{
+    //rep block of code
+}
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    // char name = 'hello world';
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%c\n",name);
+    // }
+
+
+
+    
+    // for (int i = 0; i <=5 ; i++)
+    // {
+    //     printf("hello world\n");
+    // }
+
+
+
+    
+    // for (int i = 1; i <= 20; i++)
+    // {
+    //     printf("%d\n",i);
+    // }
+
+
+
+    
+    // int table;
+    // printf("enter the number to print the table\n");
+    // scanf("%d",&table);
+
+    // for (int i=1;i<=10;i++)
+    // {
+    //     printf("%d * %d = %d\n",table,i,table*i);
+    // }
+
+
+
+
+    
+    //1  ,100 even no.
+    // 1, 100 display even and odd 
+    // display A-Z
+    //factorial
+
+
+
+
+
+
+    // for (int i = 0; i <= 100; i++)
+    // {
+    //     if (i%2==0)
+    //     {
+    //         printf("even:%d\n ",i);
+    //     }
+    //     else
+    //     {
+    //         printf("odd: %d\n ", i);
+    //     }
+        
+    // }
+
+
+    
+
+    // for (char i = 'A'; i <='Z'; i++)
+    // {
+    //     printf("%c\n",i);
+    // }
+
+
+
+
+
+    
+    // int num;
+    // printf("enter the number to find the factorial\n");
+    // scanf("%d",&num);
+    // int fact = 1;
+    // for (int i = 1; i <= num; i++)
+    // {   
+    //     fact = fact * i;
+        
+    // }
+    // printf("%d\n",fact);
+    
+    // for (int i = 0; i <= 100; i++)
+    // {
+    // if (i%2!=0)printf("%d\n",i);
+    // }
+
+
+
+
+
+
+    
+    //While loop
+    /*
+    Syntax
+    while(condition)
+    {
+        rep block code
+        exp
+    }
+    */
+
+   /*
+   int =1;
+   while (i<=10)
+   {
+    //rep block of code[printf("hello world");]
+    i++
+   }
+   */
+
+//    int i=1;
+//    while (i<=10)
+//    {
+//     printf("hello world\n");
+//     i++;
+//    }
+
+
+
+
+
+
+    //  int n1 = 15;
+    //  int n2 = 5;
+    //  int counter = 0;
+
+    //  while (n1!=n2)
+    //  {
+    //     counter++;
+    //     if(n1>n2)
+    //     {
+    //         n1 -= n2;
+    //     }
+    //     else
+    //     {
+    //         n2 -= n1;
+    //     }
+    //  }
+    //  printf("%d\n",n1);
+    //  printf("%d\n",counter);
+
+
+
+
+
+     
+     /* sun of all no. to 1 to 100*/
+
+    //  int num;
+    //  printf("enter num");
+    //  scanf("%d",&num);
+    //  int sum = 0;
+
+    // // for (int i = 1; i <= num; i++)
+    // // {
+    // //     sum = sum +i;
+    // // }
+    // // printf("%d\n",sum);
+    // int i = 1;
+    // while(i<=num)
+    // {
+    //     sum = sum + i;
+    //     i++;
+    // } 
+    // printf("%d\n",sum);
+
+
+
+
+
+    // int num;
+    // printf("enter num");
+    // scanf("%d",&num);
+    // if (num ==0)return 0;
+    // int i = 1;
+    // int sum = 1;
+    // while(i<=num)
+    // {
+    //     sum = sum + i;
+    //     i++;
+    // }
+    // printf("%d\n",sum);
+
+
+
+
+
+     /*
+     Do-while
+     syntax
+     int = 1;
+     do
+     {
+        //rep block of code
+        i++
+    }
+    while(condition);[i<=5]
+     */    
+
+    // int i= 100;
+    // do
+    // {
+    //     printf("hello world\n");
+    //     i++;
+    // } while (i<=5);
+
+
+
+    
+    // int n1, n2;
+    // printf("enter n1: ");
+    // scanf("%d", &n1);
+    // printf("enter n2 ");
+    // scanf("%d", &n2);
+    // int flage = 1;
+    // int a = n1 +n2;
+    // do{
+    //     printf("sum is %d\n", a);
+    //     printf("do you want to perform mor opertion: Y||N:");
+    //     char choice;
+    //     scanf(" %c", &choice);
+    //     if(choice == 'N')
+    //     {
+    //         flage = 0;
+    //     }
+    // }
+    // while (flage==1);
+    
+
+
+
+    return 0;
+}
